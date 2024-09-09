b3f315ab8282c2ed59e441b0c9ead1efca79aafd
Harshit Kavathia <hkavathi@umd.edu>
2024-09-09 04:07:39 -0400

    Changing path to resume

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
         <button className="start-chat-btn" onClick={startChat}>
           <FaRobot /><span>Chat about me with AI</span>
         </button>
-        <button className="download-resume-btn" onClick={() => window.open('/personal-ai/Harshit_resume.pdf', '_blank')}>
+        <button className="download-resume-btn" onClick={() => window.open('https://hkavathi.github.io/personal-ai/Harshit_resume.pdf', '_blank')}>
           <FaFileAlt /><span>Download my resume</span>
         </button>
       </div>
